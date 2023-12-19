1358a4e9c15674ab083709c1c4227cea8c2478f0
Niedzwiedz1 <niewadzilukasz@gmail.com>
2023-12-19 16:09:20 +0000

    Change mail content

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import requests
 
 from functions import send_email
 
+topic = "tesla"
 api_key = "39e5143b5e4d45ef8c515696c51c14bb"
-url = "https://newsapi.org/v2/everything?q=tesla&from=" \
-        "2023-11-19&sortBy=publishedAt&apiKey=39e5143b5e4d45ef8c515696c51c14bb"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2023-11-19&" \
+      "sortBy=publishedAt&" \
+      "apiKey=39e5143b5e4d45ef8c515696c51c14bb&language=en"
 
 request = requests.get(url)
 content = request.json()
-e_mail = f"""\
-Subject: Daily News
+e_mail = "Subject: Daily News \n"
 
-\n
-
-"""
-for article in content["articles"]:
+for article in content["articles"][:11]:
     title = article["title"]
     url = article["url"]
     news = f"{title} \n {url} \n\n"
