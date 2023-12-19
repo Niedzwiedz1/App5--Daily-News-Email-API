7ca1f462f24709f2253801bc110bf9dd1a59d265
Niedzwiedz1 <niewadzilukasz@gmail.com>
2023-12-19 15:53:20 +0000

    Add email content, send function and message encryption

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import requests
 
+from functions import send_email
+
 api_key = "39e5143b5e4d45ef8c515696c51c14bb"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-11-14&sortBy" \
-      "=publishedAt&apiKey=39e5143b5e4d45ef8c515696c51c14bb"
+url = "https://newsapi.org/v2/everything?q=tesla&from=" \
+        "2023-11-19&sortBy=publishedAt&apiKey=39e5143b5e4d45ef8c515696c51c14bb"
 
 request = requests.get(url)
 content = request.json()
+e_mail = f"""\
+Subject: Daily News
 
+\n
+
+"""
 for article in content["articles"]:
     title = article["title"]
-    print(title)
+    author = article["author"]
+    news = f"{title} '\n' {author}"
+    e_mail = e_mail + news
+
+e_mail = e_mail.encode("utf-8")
+mail = send_email(message=e_mail)
+
+
